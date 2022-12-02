5f6e55822032fcdb7f215a59e3ebaecd221b5aa7
ModyQyW <wurui-dev@foxmail.com>
2022-12-02 13:59:22 +0800

    feat!: improve types

--- types/events/index.d.ts
+++ types/events/index.d.ts
@@ -1,27 +1,42 @@
+// https://github.com/wechat-miniprogram/api-typings/blob/5dab2fb5f471f683ad297de32c0e33ede295dcff/types/wx/lib.wx.event.d.ts
+
+type AnyRecord = Record<string, any>;
+
 /**
- * @desc 触发事件的组件的一些属性值集合
+ * @desc 组件的一些属性值集合
  */
-export interface BaseEventTarget {
+export interface EventTarget<Dataset extends AnyRecord = AnyRecord> {
   /**
    * @desc 事件源组件的id
    */
   id?: string;
   /**
+   * @desc 当前组件的类型
+   */
+  tagName?: string;
+  /**
    * @desc 事件源组件上由 data- 开头的自定义属性组成的集合
    */
-  dataset?: Record<string, any>;
+  dataset?: Dataset;
+  /**
+   * @desc 距离页面顶部的偏移量
+   */
+  offsetTop: number;
+  /**
+   * @desc 距离页面左边的偏移量
+   */
+  offsetLeft: number;
   [key: string]: any;
 }
 
 /**
- * @desc 当前组件的一些属性值集合
- */
-export interface BaseEventCurrentTarget extends BaseEventTarget {}
-
-/**
  * @desc 基础事件
  */
-export interface BaseEvent {
+export interface BaseEvent<
+  Mark extends AnyRecord = AnyRecord,
+  CurrentTargetDataset extends AnyRecord = AnyRecord,
+  TargetDataset extends AnyRecord = CurrentTargetDataset,
+> {
   /**
    * @desc 事件类型
    */
@@ -31,35 +46,40 @@ export interface BaseEvent {
    */
   timeStamp?: number;
   /**
-   * @desc 触发事件的组件的一些属性值集合
+   * @desc 事件冒泡路径上所有由 mark: 开头的自定义属性组成的集合
    */
-  target?: BaseEventTarget<T>;
+  mark?: Mark;
   /**
-   * @desc 当前组件的一些属性值集合
+   * @desc 触发事件的源组件的一些属性值集合
    */
-  currentTarget?: BaseEventCurrentTarget;
+  target?: EventTarget<TargetDataset>;
   /**
-   * @desc 事件标记数据
+   * @desc 事件绑定的当前组件的一些属性值集合
    */
-  mark?: Record<string, any>;
+  currentTarget?: Target<CurrentTargetDataset>;
   [key: string]: any;
 }
 
 /**
  * @desc 自定义事件
  */
-export interface CustomEvent<T = Record<string, any>> extends BaseEvent {
+export interface CustomEvent<
+  Detail extends AnyRecord = AnyRecord,
+  Mark extends AnyRecord = AnyRecord,
+  CurrentTargetDataset extends AnyRecord = AnyRecord,
+  TargetDataset extends AnyRecord = CurrentTargetDataset,
+> extends BaseEvent<Mark, CurrentTargetDataset, TargetDataset> {
   /**
    * @desc 额外信息
    */
-  detail?: T;
+  detail?: Detail;
   [key: string]: any;
 }
 
 /**
  * @desc 当前停留在屏幕中的触摸点信息
  */
-export interface TouchEventTouch {
+export interface TouchDetail {
   /**
    * @desc 标志符
    */
@@ -85,7 +105,7 @@ export interface TouchEventTouch {
 /**
  * @desc 当前停留在 canvas 中的触摸点信息
  */
-export interface TouchEventCanvasTouch {
+export interface TouchCanvasDetail {
   /**
    * @desc 标志符
    */
@@ -101,19 +121,41 @@ export interface TouchEventCanvasTouch {
 }
 
 /**
- * @desc 当前变化的屏幕触摸点信息
- */
-export interface TouchEventChangedTouch extends TouchEventTouch {}
-
-/**
- * @desc 当前变化的 canvas 触摸点信息
- */
-export interface TouchEventCanvasChangedTouch extends TouchEventCanvasTouch {}
-
-/**
  * @desc 触摸事件
  */
-export interface TouchEvent<T = TouchEventTouch, D = TouchEventChangedTouch> extends BaseEvent {
+export interface BaseTouchEvent<
+  Detail extends AnyRecord = AnyRecord,
+  T extends TouchDetail | TouchCanvasDetail = TouchDetail,
+  Mark extends AnyRecord = AnyRecord,
+  CurrentTargetDataset extends AnyRecord = AnyRecord,
+  TargetDataset extends AnyRecord = CurrentTargetDataset,
+> extends CustomEvent<Detail, Mark, CurrentTargetDataset, TargetDataset> {
+  /**
+   * @desc 当前停留在屏幕中的触摸点信息的数组
+   */
   touches: T[];
-  changedTouches: D[];
+  /**
+   * @desc 当前变化的触摸点信息的数组
+   */
+  changedTouches: T[];
+}
+
+/**
+ * @desc 触摸事件响应
+ */
+export type TouchEvent<
+  Detail extends AnyRecord = AnyRecord,
+  Mark extends AnyRecord = AnyRecord,
+  CurrentTargetDataset extends AnyRecord = AnyRecord,
+  TargetDataset extends AnyRecord = CurrentTargetDataset,
+> = BaseTouchEvent<Detail, TouchDetail, Mark, CurrentTargetDataset, TargetDataset>;
+
+/**
+ * @desc canvas 触摸事件响应
+ */
+interface TouchCanvasEvent<
+  Mark extends AnyRecord = AnyRecord,
+  TargetDataset extends AnyRecord = AnyRecord,
+> extends BaseTouchEvent<never, TouchCanvasDetail, Mark, never, TargetDataset> {
+  currentTarget: never;
 }
