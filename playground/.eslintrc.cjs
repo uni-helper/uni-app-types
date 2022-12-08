cf76859d15ceb319de078c1dbf4dc3f89d19bd77
ModyQyW <wurui-dev@foxmail.com>
2022-12-08 12:57:43 +0800

    chore: add playground

---
+++ playground/.eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('@modyqyw/fabric/eslint/vanilla'),
+    require.resolve('@modyqyw/fabric/eslint/typescript'),
+    require.resolve('@modyqyw/fabric/eslint/vue'),
+  ],
+};
