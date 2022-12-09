4ed6a2cd767f75626f0fc7a346b7b39a6d37671b
ModyQyW <wurui-dev@foxmail.com>
2022-12-09 14:54:57 +0800

    chore: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
+import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('types', import.meta.url)),
+    },
+  },
   test: {
     coverage: {
       provider: 'c8',
