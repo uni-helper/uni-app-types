270072691a86efafebbfdb8ef168e57f3458586b
ModyQyW <wurui-dev@foxmail.com>
2023-01-06 22:33:19 +0800

    chore: update

--- playground/.eslintrc.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  root: true,
-  extends: [
-    require.resolve('@modyqyw/fabric/eslint/vanilla'),
-    require.resolve('@modyqyw/fabric/eslint/typescript'),
-    require.resolve('@modyqyw/fabric/eslint/vue'),
-  ],
-};
