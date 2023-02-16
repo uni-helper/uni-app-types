742aa1b9fc019da8377c54c56e08cac293f2ad5e
ModyQyW <wurui-dev@foxmail.com>
2023-02-16 18:43:37 +0800

    chore: update config

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
   '*.md': 'markdownlint --fix',
-  './types/**/*.{js,cjs,mjs,ts,cts,mts}': 'eslint --fix --cache --no-error-on-unmatched-pattern',
+  './types/**/*.{js,cjs,mjs,ts,cts,mts}': 'eslint --fix --cache',
 };
