802ea33cb0a232021e0aed69de5eb8d2be3c760b
ModyQyW <wurui-dev@foxmail.com>
2023-03-06 20:46:12 +0800

    chore: path

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.md': 'markdownlint --fix --ignore=CHANGELOG.md --ignore-path=./gitignore',
-  './src/**/*.{js,cjs,mjs,ts,cts,mts}': 'eslint --fix --cache --ignore-path=.gitginore',
+  '*.md': 'markdownlint --fix --ignore=CHANGELOG.md --ignore-path=.gitignore',
+  './src/**/*.{js,cjs,mjs,ts,cts,mts}': 'eslint --fix --cache --ignore-path=.gitignore',
 };
