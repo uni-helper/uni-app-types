df2a7b647a536a5719d9523c951e25992cac2ea3
ModyQyW <wurui-dev@foxmail.com>
2023-05-17 10:48:31 +0800

    chore: update playground

---
+++ playground/vue2.6/src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  onLaunch() {
+    console.log('App Launch');
+  },
+  onShow() {
+    console.log('App Show');
+  },
+  onHide() {
+    console.log('App Hide');
+  },
+});
+</script>
